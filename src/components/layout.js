d86c6d347b25568f4596a3974697c930ef44f58f
Dan Weaver <daniel.weaver@gmail.com>
2019-05-06 12:11:16 -0600

    remove ga

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,14 +7,6 @@ import Header from "./header"
 export default ({ children }) => (
   <div id="snf">
     <Helmet>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139719124-1"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-139719124-1');
-      </script>
       <title>Supernatural Feat</title>
       {/* <link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet" /> */}
       <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
